553cb1a50d642cb3775cbb79f72e730f3e5c7523
xnaut97 <xnaut97@gmail.com>
2024-05-10 19:48:37 +0700

    Update

--- src/main/java/com/github/tezvn/lunix/item/FloatingItem.java
+++ src/main/java/com/github/tezvn/lunix/item/FloatingItem.java
@@ -1,20 +1,28 @@
 package com.github.tezvn.lunix.item;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
-import org.bukkit.ChatColor;
+import lombok.Getter;
 import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Vector;
 
 
 /**
  * A class that manages functions relating to {@link ItemStack} that floating in
  * {@link Item} form.
  */
+@Getter
 public class FloatingItem {
 
     /**
@@ -28,33 +36,19 @@ public class FloatingItem {
      * Find a {@link FloatingItem} with given ID near the given {@link Location}.
      */
     public static Optional<FloatingItem> find(Location location, String ID) {
-        for (Entity entity : location.getWorld().getNearbyEntities(location, 1, 1.5, 1)) {
-            if(!(entity instanceof Item))
-                continue;
-            Item item = (Item) entity;
-            if (item.getCustomName() != null
-                    && item.getItemStack().getItemMeta().getDisplayName().startsWith(ID.toUpperCase())) {
-                ItemStack itemStack = item.getItemStack();
-                ItemStack stack = itemStack.clone();
-                String customName = item.getCustomName();
-                if (customName.equals("")) {
-                    ItemMeta im = stack.getItemMeta();
-                    im.setDisplayName(null);
-                    stack.setItemMeta(im);
-                } else {
-                    ItemMeta im = stack.getItemMeta();
-                    if (!customName.startsWith(""))
-                        customName = ChatColor.RESET + customName;
-                    im.setDisplayName(customName);
-                    stack.setItemMeta(im);
-                }
-
-                FloatingItem floatingItem = new FloatingItem(ID, stack);
-                floatingItem.setItem(item);
-                return Optional.ofNullable(floatingItem);
-            }
+        AtomicReference<Optional<FloatingItem>> optional = new AtomicReference<>(Optional.empty());
+        World world = location.getWorld();
+        if(world != null) {
+            world.getNearbyEntities(location, 1, 1, 1).stream()
+                    .filter(item -> item instanceof Item && item.hasMetadata("id") && item.getMetadata("id").get(0).asString().equals(ID))
+                    .map(entity -> (Item) entity)
+                    .findFirst().ifPresent(item -> {
+                        String id = item.getMetadata("id").get(0).asString();
+                        ItemStack stack = item.getItemStack();
+                        optional.set(Optional.of(FloatingItem.of(id, stack)));
+                    });
         }
-        return Optional.empty();
+        return optional.get();
     }
 
     /**
@@ -78,44 +72,41 @@ public class FloatingItem {
         this.itemStack = itemStack;
     }
 
-    public String getID() {
-        return ID;
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
     private void setItem(Item item) {
         this.item = item;
-    }
-
-    public ItemStack getItemStack() {
-        return itemStack;
     }
 
     /**
      * Summon {@link FloatingItem} above the given {@link Block}.
      */
-    public void summon(Block block) {
-//        ItemStack clone = getItemStack().clone();
-//        clone.setAmount(1);
-//        String nametag = StringUtils.formatItemName(clone);
-//        Item entity = block.getWorld().dropItem(block.getLocation().add(0.5, 1.2, 0.5),
-//                new ItemBuilder(clone).setDisplayName(getID().toUpperCase() + " &e" + System.nanoTime()).create());
-//        entity.setVelocity(new Vector(0, 0.1, 0));
-//        entity.setMetadata("no_pickup", new FixedMetadataValue(TCore.getInstance(), "no_pickup"));
-//        entity.setCustomNameVisible(true);
-//        entity.setCustomName(nametag);
-//        setItem(entity);
-//        block.getWorld().playSound(block.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.3F, 0.3F);
+    public void summon(Block block, Plugin plugin) {
+        ItemStack clone = getItemStack().clone();
+        clone.setAmount(1);
+        Item entity = block.getWorld().dropItem(block.getLocation().add(0.5, 1.2, 0.5), clone);
+        entity.setVelocity(new Vector(0, 0.1, 0));
+        entity.setCustomNameVisible(true);
+        entity.setPickupDelay(Integer.MAX_VALUE);
+        entity.setCustomName(getDisplayName());
+        entity.setMetadata("id", new FixedMetadataValue(plugin, getID()));
+        setItem(entity);
+        block.getWorld().playSound(block.getLocation(), Sound.ENTITY_ITEM_PICKUP, 0.3F, 0.3F);
     }
 
     /**
      * Remove the {@link FloatingItem}.
      */
     public void remove() {
-        getItem().remove();
+        if (getItem() != null)
+            getItem().remove();
+    }
+
+    private String getDisplayName() {
+        ItemMeta meta = getItemStack().getItemMeta();
+        return meta != null && meta.hasDisplayName()
+                ? meta.getDisplayName()
+                : Arrays.stream(getItemStack().getType().name().split("_"))
+                .map(s -> s.charAt(0) + s.substring(1))
+                .collect(Collectors.joining(" "));
     }
 
 }
